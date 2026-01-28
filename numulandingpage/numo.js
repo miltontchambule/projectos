abd8aa8fd61f95fc4e5ad2c926de49fa10bc6e7e
Milton <you@example.com>
2026-01-28 23:05:25 +0200

    restruturacao da landing page numo

--- numulandingpage/numo.js
+++ numulandingpage/numo.js
@@ -1,10 +1,10 @@
 const elementos = document.querySelectorAll('.animar');
 
 function animarAoScroll() {
-    const limite = window.innerHeight * 0.85;
+    const limite = window.innerHeight * 0.85; //os elementos aparecem dentro de 85% de viewport.
 
     elementos.forEach(el => {
-        const topo = el.getBoundingClientRect().top;
+        const topo = el.getBoundingClientRect().top; //medicao do topo da tela ate ao elemento com animacao.
         if (topo < limite) {
             el.classList.add('visivel');
         }
