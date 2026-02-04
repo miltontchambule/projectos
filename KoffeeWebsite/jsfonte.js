11268711577cb0cd8278f2e43d6bb3c14a8f0104
Milton <you@example.com>
2026-02-05 00:58:58 +0200

    melhorias no wbesite koffee

--- KoffeeWebsite/jsfonte.js
+++
@@ -1,8 +0,0 @@
-// menu hamburguer
-
-const botaoMenu = document.getElementById('botao-menu');
-const menu = document.querySelector('.menu');
-botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    botaoMenu.classList.toggle('ativo');
-});
