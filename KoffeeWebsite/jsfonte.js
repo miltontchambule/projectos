8ad50b279e76e7549ed0587e3023c719cb9fff4c
Milton <you@example.com>
2026-02-08 03:26:01 +0200

    fixed the navbar for ios

--- KoffeeWebsite/jsfonte.js
+++ KoffeeWebsite/jsfonte.js
@@ -7,38 +7,9 @@ botaoMenu.addEventListener('click', () => {
     botaoMenu.classList.toggle('ativo');
 });
 
-const MAP_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3587.1259890966217!2d32.5617991742074!3d-25.963908077225327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1ee69b7d6166b651%3A0xe324aea2cffbd8b5!2sKoffee%20Shop.%20EI!5e0!3m2!1spt-PT!2smz!4v1770216729289!5m2!1spt-PT!2smz";
+// const MAP_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3587.1259890966217!2d32.5617991742074!3d-25.963908077225327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1ee69b7d6166b651%3A0xe324aea2cffbd8b5!2sKoffee%20Shop.%20EI!5e0!3m2!1spt-PT!2smz!4v1770216729289!5m2!1spt-PT!2smz";
 
-if (userAcceptedCookies) {
-    document.getElementById('map').src = MAP_URL;
-};
-
-gsap.registerPlugin(ScrollTrigger);
-const navbar = document.querySelector(".navbar");
-const logo = document.querySelector(".logo");
-
-function setModoClaro(ativo) {
-    navbar.classList.toggle("modo-claro", ativo);
-    logo.style.opacity = 0;
-    
-    setTimeout(() => {
-        logo.src = ativo
-        ? "/Media/blacklogo.png"
-        : "/Media/Logokoffee160.png";
-        logo.style.opacity = 1;
-    }, 150);
-}
-
-//sempre que navbar passar por uma zona clara da pagina
-
-ScrollTrigger.create({
-    trigger: ".clara", 
-    start: "top 20%",
-    end: "bottom 20%",
-
-    onEnter: () => setModoClaro(true),
-    onLeave: () => setModoClaro(false),
-    onEnterBack: () => setModoClaro(true),
-    onLeaveBack: () => setModoClaro(false) 
-});
+// if (userAcceptedCookies) {
+//    document.getElementById('map').src = MAP_URL;
+// };
 
