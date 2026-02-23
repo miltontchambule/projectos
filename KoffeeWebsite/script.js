ec218e891e4d2ff603e2cc45a02ea79407d6f281
Milton <you@example.com>
2026-02-24 00:18:22 +0200

    menu hamburguer
    problema do cta de encomendar no iphone

---
+++ KoffeeWebsite/script.js
@@ -0,0 +1,19 @@
+//MENU HAMBURGUER 
+
+ function toggleMenu() {
+    var menu    = document.getElementById('mobileMenu');
+    var overlay = document.getElementById('menuOverlay');
+    var burger  = document.getElementById('hamburger');
+    menu.classList.toggle('open');
+    overlay.classList.toggle('active');
+    burger.classList.toggle('active');
+  }
+
+  function closeMenu() {
+    var menu    = document.getElementById('mobileMenu');
+    var overlay = document.getElementById('menuOverlay');
+    var burger  = document.getElementById('hamburger');
+    menu.classList.remove('open');
+    overlay.classList.remove('active');
+    burger.classList.remove('active');
+  }
